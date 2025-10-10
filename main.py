7f3cae88f371b06edd8ee80664444f7d56faea1b
miguelvitta <miguelvittadev@gmail.com>
2025-10-10 19:48:37 -0300

    feat: a function that gets the book text and prints it to the stdout.

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("greetings boots")
+
+def main():
+    book_path = ("./books/frankenstein.txt")
+    file_contents = get_book_text(f"{book_path}")
+    print(file_contents)
+
+def get_book_text(book_path):
+    file_contents = ""
+    with open(book_path) as file:
+        file_contents = file.read()
+    return file_contents
+
+main()
