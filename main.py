f4d3b65ac4a8a65de556b83decf4c92b4bd31afc
miguelvitta <miguelvittadev@gmail.com>
2025-10-19 12:21:57 -0300

    feat: the final version with 3 books to test

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from stats import get_num_words, get_book_text, get_char_count, sort_dictionary
+from stats import get_num_words, get_char_count, sort_dictionary
+import sys
 
 def main():
-    path = ("./books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    path = sys.argv[1]
     num_words = get_num_words(path)
     # print(f"Found {num_words} total words")
     char_count = get_char_count(path)
